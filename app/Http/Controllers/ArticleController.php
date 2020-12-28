c1dcdd206d0b52bbe2bc3218e5ff6052b0b0eb34
wynadiis23 <adii.empty@gmail.com>
2020-12-28 11:40:48 +0800

    api sudah rest

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -58,4 +58,94 @@ class ArticleController extends Controller
             ]);
         }
     }
+    /**
+     * show article by id
+     */
+    public function show($id)
+    {
+        try{
+            $article = Article::findOrFail($id);
+            /**
+             * bagian ini tidak bekerja
+             * laravel langsung route ke page 404ketika tidak ada data sesuai id
+             * edit di bagian app\Exceptions\Handler.php 
+             */
+            if($article === null) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Article not found with this id',
+                    'data' => $article
+                ]);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Article found, here it is',
+                'data' => $article
+            ]);
+
+        } catch(Exception $error) {
+            return response()->json([
+                'message' => 'Error in finding article',
+                'error' => $error,
+            ]);
+        }
+    }
+
+    /**
+     * update function untuk artikel 
+     */
+    public function update(Request $request, $id)
+    {   
+        try{
+            $request->validate([
+                'title' => 'required',
+                'author' => 'required'
+            ]);
+            //find article by id
+            $article = Article::findOrFail($id);
+
+            //update article properties
+            $article->title = $request->title;
+            $article->author = $request->author;
+            
+            //savearticle
+            if($article->save()) {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Article data successfully updated',
+                    'data' => $article
+                ]);
+            }
+            
+
+        } catch(Exception $error) {
+            return response()->json([
+                'message' => 'Error in update article',
+                'error' => $error,
+            ]);
+        }
+    }
+
+    /**
+     * delete data article
+     */
+    public function destroy($id)
+    {
+        try{
+            $article = Article::findOrFail($id);
+
+            if($article->delete()){
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Article data successfully deleted'
+                ]);
+            }
+        } catch(Exception $error) {
+            return response()->json([
+                'message' => 'Error in deleting article',
+                'error' => $error,
+            ]);
+        }
+    }
 }
